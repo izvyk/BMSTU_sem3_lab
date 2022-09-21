299224326ce1edeb0a72a01b56d2dcce2c383bb3
Philipp Mukosey <philipp@mukosey.com>
2022-09-21 16:23:12 +0300

    lab3 completed

---
+++ 3/doc_elements_list.js
@@ -0,0 +1,15 @@
+function show_page_tree(element=document.body, depth=0) {
+    var tree = document.getElementById("tree");
+    tree.append('\xa0'.repeat(depth * 4) + element.nodeName + '\n');  
+    element.childNodes.forEach(function (i) {
+        if (i.id != "tree-container") {
+            show_page_tree(i, depth + 1);
+        }
+    })
+    if (element.nodeType == 1)
+        tree.append('\xa0'.repeat(depth * 4) + '/' + element.nodeName + '\n');
+}
+
+function show_tree_btn_disable() {
+    document.getElementById("show-tree-btn").disabled = true;
+}
