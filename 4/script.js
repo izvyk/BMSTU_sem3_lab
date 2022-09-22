e2f87c029fde1f92324f503e3e3fe8f3d7cc9904
Philipp Mukosey <philipp@mukosey.com>
2022-09-22 20:23:57 +0300

    lab4 started

---
+++ 4/script.js
@@ -0,0 +1,12 @@
+function count() {
+
+}
+
+function update_cost() {
+    
+}
+
+function add_item() {
+    let new_row = document.getElementById("items-table").insertRow(-1);
+    new_row.insertCell(text("test"));
+}
